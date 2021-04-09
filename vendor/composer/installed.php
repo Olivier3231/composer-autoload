0f07d8eefc189056b2dd0e4c218a2fa6263545ff
Olivier3231 <maffrolivier74@gmail.com>
2021-04-09 12:22:50 +0200

    quete composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'olivier/quete_comoser',
+  ),
+  'versions' => 
+  array (
+    'olivier/quete_comoser' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
